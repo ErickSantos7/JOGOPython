a1f02b474bbbac9598c18f39d6e207ee10f68947
Erick <erickribeirodo@gmail.com>
2026-03-20 00:36:31 -0300

    initial commit

---
+++ code/Game.py
@@ -0,0 +1,18 @@
+import pygame
+
+from code.Const import WIN_WIDTH, WIND_HEIGHT
+from code.Menu import Menu
+
+
+class Game:
+
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIND_HEIGHT))
+
+    def run(self):
+
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+
